3e39306df11067d13546245ff9738bfa19829735
WNagorny <walery.nagorny@gmail.com>
2023-06-20 15:03:57 +0200

    added sidebar section

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,98 @@
+import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top"><span className="logo">VangAdmin</span></div>
+
+      <hr />
+      <div className="center">
+         <ul>
+            <p className="title">Main</p>
+            <li>
+               <DashboardIcon className='icon'/>
+               <span>Dashboard</span>
+            </li>
+
+            <p className="title">Lists</p>
+            <li>
+               <PersonOutlineOutlinedIcon className='icon'/>
+               <span>Users</span>
+            </li>
+
+            
+            <li>
+               <StoreIcon className='icon'/>
+               <span>Products</span>
+            </li>
+
+            
+            <li>
+               <CreditCardIcon  className='icon'/>
+               <span>Orders</span>
+            </li>
+            <li>
+               <LocalShippingIcon className='icon'/>
+               <span>Delivery</span>
+            </li>
+
+            <p className="title">Useful</p>
+            <li>
+               <InsertChartIcon className='icon'/>
+               <span>Stats</span>
+            </li>
+            <li>
+               <NotificationsActiveIcon className='icon'/>
+               <span>Notifications</span>
+            </li>
+
+            <p className="title">Service</p>
+            <li>
+               <SettingsSystemDaydreamIcon className='icon'/>
+               <span>System Health</span>
+            </li>
+            <li>
+               <PsychologyOutlinedIcon className='icon'/>
+               <span>Logs</span>
+            </li>
+            <li>
+               <SettingsApplicationsIcon  className='icon'/>
+               <span>Settings</span>
+            </li>
+
+            <p className="title">User</p>
+            <li>
+               <AccountCircleOutlinedIcon  className='icon'/>
+               <span>Profile</span>
+            </li>
+            <li>
+               <ExitToAppIcon className='icon'/>
+               <span>Log out</span>
+            </li>
+         </ul>
+      </div>
+
+
+      <div className="bottom">
+
+         <div className="colorOption"></div>
+         <div className="colorOption"></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
