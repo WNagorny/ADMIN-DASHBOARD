77ff15f30f6bdb9558d3f35d80aaf1ee29e71c12
WNagorny <walery.nagorny@gmail.com>
2023-06-22 15:35:25 +0200

    configuration links on Dashboard

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,36 +1,48 @@
 import './datatable.scss'
+import {Link} from 'react-router-dom'
 
-import { DataGrid } from '@mui/x-data-grid';
-import {userColumns, userRows} from '../../datatablesource'
-
+import { DataGrid } from '@mui/x-data-grid'
+import { userColumns, userRows } from '../../datatablesource'
 
 const Datatable = () => {
-
-   const actionColumn = [
-      {field:"action", headerName:"Action", width:200, renderCell:()=>{
-         return (
-            <div className="cellAction">
-               <div className="viewButton">View</div>
-               <div className="deleteButton">Delete</div>
-            </div>
-         )
-      }}
-   ]
-  return (
-    <div className='datatable'>
-      <DataGrid
-        rows={userRows}
-        columns={userColumns.concat(actionColumn)}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 8 },
-          },
-        }}
-        pageSizeOptions={[4, 4]}
-        checkboxSelection
-      />
-    </div>
-  )
+	const actionColumn = [
+		{
+			field: 'action',
+			headerName: 'Action',
+			width: 200,
+			renderCell: () => {
+				return (
+					<div className='cellAction'>
+						<Link to='/users/test' style={{ textDecoration: 'none' }}>
+							<div className='viewButton'>View</div>
+						</Link>
+						<div className='deleteButton'>Delete</div>
+					</div>
+				)
+			},
+		},
+	]
+	return (
+		<div className='datatable'>
+      <div className="datatableTitle">
+        All Users List
+        <Link to='/users/new' style={{ textDecoration: 'none' }} className='link'>
+          Add New User
+        </Link>
+      </div>
+			<DataGrid
+				rows={userRows}
+				columns={userColumns.concat(actionColumn)}
+				initialState={{
+					pagination: {
+						paginationModel: { page: 0, pageSize: 8 },
+					},
+				}}
+				pageSizeOptions={[4, 4]}
+				checkboxSelection
+			/>
+		</div>
+	)
 }
 
 export default Datatable
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,98 +1,101 @@
 import './sidebar.scss'
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import StoreIcon from '@mui/icons-material/Store';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
-import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import InsertChartIcon from '@mui/icons-material/InsertChart';
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
+import StoreIcon from '@mui/icons-material/Store'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream'
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+import InsertChartIcon from '@mui/icons-material/InsertChart'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-      <div className="top"><span className="logo">VangAdmin</span></div>
+	return (
+		<div className='sidebar'>
+			<Link to='/' style={{ textDecoration: 'none' }}>
+				<div className='top'>
+					<span className='logo'>VangAdmin</span>
+				</div>
+			</Link>
 
-      <hr />
-      <div className="center">
-         <ul>
-            <p className="title">Main</p>
-            <li>
-               <DashboardIcon className='icon'/>
-               <span>Dashboard</span>
-            </li>
+			<hr />
+			<div className='center'>
+				<ul>
+					<p className='title'>Main</p>
+					<li>
+						<DashboardIcon className='icon' />
+						<span>Dashboard</span>
+					</li>
 
-            <p className="title">Lists</p>
-            <li>
-               <PersonOutlineOutlinedIcon className='icon'/>
-               <span>Users</span>
-            </li>
+					<p className='title'>Lists</p>
+					<Link to='/users' style={{ textDecoration: 'none' }}>
+						<li>
+							<PersonOutlineOutlinedIcon className='icon' />
+							<span>Users</span>
+						</li>
+					</Link>
 
-            
-            <li>
-               <StoreIcon className='icon'/>
-               <span>Products</span>
-            </li>
+					<Link to='/products' style={{ textDecoration: 'none' }}>
+						<li>
+							<StoreIcon className='icon' />
+							<span>Products</span>
+						</li>
+					</Link>
+					<li>
+						<CreditCardIcon className='icon' />
+						<span>Orders</span>
+					</li>
+					<li>
+						<LocalShippingIcon className='icon' />
+						<span>Delivery</span>
+					</li>
 
-            
-            <li>
-               <CreditCardIcon  className='icon'/>
-               <span>Orders</span>
-            </li>
-            <li>
-               <LocalShippingIcon className='icon'/>
-               <span>Delivery</span>
-            </li>
+					<p className='title'>Useful</p>
+					<li>
+						<InsertChartIcon className='icon' />
+						<span>Stats</span>
+					</li>
+					<li>
+						<NotificationsActiveIcon className='icon' />
+						<span>Notifications</span>
+					</li>
 
-            <p className="title">Useful</p>
-            <li>
-               <InsertChartIcon className='icon'/>
-               <span>Stats</span>
-            </li>
-            <li>
-               <NotificationsActiveIcon className='icon'/>
-               <span>Notifications</span>
-            </li>
+					<p className='title'>Service</p>
+					<li>
+						<SettingsSystemDaydreamIcon className='icon' />
+						<span>System Health</span>
+					</li>
+					<li>
+						<PsychologyOutlinedIcon className='icon' />
+						<span>Logs</span>
+					</li>
+					<li>
+						<SettingsApplicationsIcon className='icon' />
+						<span>Settings</span>
+					</li>
 
-            <p className="title">Service</p>
-            <li>
-               <SettingsSystemDaydreamIcon className='icon'/>
-               <span>System Health</span>
-            </li>
-            <li>
-               <PsychologyOutlinedIcon className='icon'/>
-               <span>Logs</span>
-            </li>
-            <li>
-               <SettingsApplicationsIcon  className='icon'/>
-               <span>Settings</span>
-            </li>
+					<p className='title'>User</p>
+					<li>
+						<AccountCircleOutlinedIcon className='icon' />
+						<span>Profile</span>
+					</li>
+					<li>
+						<ExitToAppIcon className='icon' />
+						<span>Log out</span>
+					</li>
+				</ul>
+			</div>
 
-            <p className="title">User</p>
-            <li>
-               <AccountCircleOutlinedIcon  className='icon'/>
-               <span>Profile</span>
-            </li>
-            <li>
-               <ExitToAppIcon className='icon'/>
-               <span>Log out</span>
-            </li>
-         </ul>
-      </div>
-
-
-      <div className="bottom">
-
-         <div className="colorOption"></div>
-         <div className="colorOption"></div>
-
-      </div>
-    </div>
-  )
+			<div className='bottom'>
+				<div className='colorOption'></div>
+				<div className='colorOption'></div>
+			</div>
+		</div>
+	)
 }
 
 export default Sidebar
